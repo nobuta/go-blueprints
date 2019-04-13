891697686a89b42965efc7a1484a2ebd4a9d8a3a
nobuta <dev44nobu@gmail.com>
2019-04-13 20:56:56 +0900

    use Handler

--- chat/main.go
+++ chat/main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"runtime"
+	"sync"
 )
 
+type templateHandler struct {
+	once 	sync.Once
+	file 	string
+	tmpl 	*template.Template
+}
+
+func current() string {
+	_, file, _, _ := runtime.Caller(1)
+	return filepath.Dir(file)
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		path := filepath.Join( current(), "templates", t.file)
+		fmt.Println(path)
+		t.tmpl = template.Must(template.ParseFiles(path))
+	})
+	_ = t.tmpl.Execute(w, nil)
+}
+
 func main() {
-	http.HandleFunc(
-		"/",
-		func(writer http.ResponseWriter, request *http.Request) {
-			writer.Write([]byte("<html>chat</html>"))
-		})
+	http.Handle("/", &templateHandler{file:"index.html"})
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
